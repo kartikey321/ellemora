c1675e07c2a3003c4696bf5d25640922311fb974
kartikey321 <kartikeymahawar1234@gmail.com>
2024-03-02 20:04:44 +0530

    initial commit

---
+++ src/screens/mobile/Home/components/itemCard/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ItemCard() {
+  const items = [
+    {
+      name: "PINK CASCADE SET",
+      image: "url()",
+      price: "",
+    },
+  ];
+  return <div className="body"></div>;
+}
+
+export default ItemCard;
